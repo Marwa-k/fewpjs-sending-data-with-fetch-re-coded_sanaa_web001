013bf3a13b7b609dc12d0e41fd3666a1552055a5
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-09-13 14:21:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Add your code here
+
+function submitData(userName,userEmail)
+{
+  
+  let DataSend={
+    name:userName,
+    email:userEmail
+  }
+  
+  let DataObject={
+    
+    method: "POST", 
+     headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+    },
+      body: JSON.stringify(DataSend)
+   }
+  
+  return fetch('http://localhost:3000/users',DataObject)
+  .then(function(response) {
+    return response.json();
+     })
+   .then(function(object) {
+     const body = document.getElementsByTagName('body');  
+     var p = document.createElement('p');
+      p.innerHTML = object.id;
+      body[0].appendChild(p);          
+       console.log(object.id);
+      })
+    .catch(function(error) {
+      alert("Bad things! Ragnarők!");
+       const body = document.getElementsByTagName('body');  
+       var p = document.createElement('p');
+       p.innerHTML = error.message;
+       body[0].appendChild(p);                    
+         console.log(error.message);
+        });
+ 
+}
+  submitData('Marwa','m@gmail.com')
+   submitData('sondos','s@gmail.com')
